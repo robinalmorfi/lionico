b0f4d6ab32b19540c0e2dfb38605af019c6ed14c
robinalmorfi <robin.almorfi2002@gmail.com>
2022-11-24 17:47:22 +0800

    initial

---
+++ customer/gallery.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Gallery</title>
+</head>
+<body>
+<header>
+    <div class="container">
+        <div class="label">
+            <img src="../img/lion2.png" alt="lion-head" id="lion">
+            <span class="logo-name">LIONICO BARBER</span>
+        </div>
+
+        <nav>
+            <ul class="menu">
+                <li><a href="../customer/home.php">Home</a></li>
+                <li><a href="../customer/services.php">Services</a></li>
+                <li><a href="../customer/gallery.php">Gallery</a></li>
+                <li><a href="../customer/calendar.php">Calendar</a></li>
+                <li><a href="../customer/reviews.php">Reviews</a></li>
+                <li><a href="../customer/about.php">About</a></li>
+                <li><a href="../login/logout.php">Logout</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+</body>
+</html>
--- customer/home.php
+++ customer/home.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
-    <title>Customer</title>
+    <title>Home</title>
 </head>
 <body>
 <header>
@@ -17,16 +17,55 @@
 
         <nav>
             <ul class="menu">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Gallery</a></li>
-                <li><a href="#">Calendar</a></li>
-                <li><a href="#">Reviews</a></li>
-                <li><a href="#">About</a></li>
+                <li><a href="../customer/home.php">Home</a></li>
+                <li><a href="../customer/services.php">Services</a></li>
+                <li><a href="../customer/gallery.php">Gallery</a></li>
+                <li><a href="../customer/calendar.php">Calendar</a></li>
+                <li><a href="../customer/reviews.php">Reviews</a></li>
+                <li><a href="../customer/about.php">About</a></li>
                 <li><a href="../login/logout.php">Logout</a></li>
             </ul>
         </nav>
     </div>
 </header>
+<main>
+      <div class="homecontent">
+        <div class="container">  
+          <h1>Want a Hair Cut?</h1>
+    </main>
+
+    <section class="sections">
+      <div class="container">
+        
+        <div class="col">
+          <img src="../img/686317.png">
+          <h2>See Services</h2>
+          <p>Take a look at our services</p>
+          <a href="../customer/services.php">Click here</a><br>
+        </div>
+        
+        <div class="col">
+          <img src="../img/3342176.png">
+          <h2>See Gallery</h2>
+          <p>See our establishment.</p>
+          <a href="../customer/gallery.php">Click here</a><br>
+        </div>
+        
+        <div class="col">
+          <img src="../img/42446.png">
+          <h2>See Calendar</h2>
+          <p>View our calendar.</p>
+          <a href="../customer/calendar.php">Click here</a><br>
+        </div>
+        
+      </div>
+    </section>
+
+    <footer>
+      <div class="container">
+        <p>&copy; Lionico Barder Shop | Ayala Zamboanga City | 0905 188 4862</p>
+      </div>
+    </footer>
+
 </body>
 </html>
