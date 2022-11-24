d5795c32caffb38c91543d12dc67c3a22ed5e8dc
robinalmorfi <robin.almorfi2002@gmail.com>
2022-11-24 22:23:43 +0800

    initial

--- customer/calendar.php
+++ customer/calendar.php
@@ -28,5 +28,152 @@
         </nav>
     </div>
 </header>
+<br />
+<section class="calendar">
+      <div class="container">
+        <h2>November 2022</h2>
+    <br />
+      
+    <table bgcolor="lightgrey" align="center" width="300"
+        cellspacing="21" cellpadding="21"> 
+        <caption align="top">
+        </caption>
+        <thead>
+            <tr>
+
+                <th>Sun</th>
+                <th>Mon</th>
+                <th>Tue</th>
+                <th>Wed</th>
+                <th>Thu</th>
+                <th>Fri</th>
+                <th>Sat</th>
+            </tr>
+        </thead>
+          
+        <tbody>
+            <tr>
+                <td> </td>
+                <td> </td>
+                <td>1</td>
+                <td>2</td>
+                <td>3</td>
+                <td>4</td>
+                <td>5</td>
+            </tr>
+            <tr>
+                <td>6</td>
+                <td>7</td>
+                <td>8</td>
+                <td>9</td>
+                <td>10</td>
+                <td>11</td>
+                <td>12</td>
+            </tr>
+            <tr>
+                <td>13</td>
+                <td>14</td>
+                <td>15</td>
+                <td>16</td>
+                <td>17</td>
+                <td>18</td>
+                <td>19</td>
+            </tr>
+            <tr>
+                <td>20</td>
+                <td>21</td>
+                <td>22</td>
+                <td>23</td>
+                <td>24</td>
+                <td>25</td>
+                <td>26</td>
+            </tr>
+            <tr>
+                <td>27</td>
+                <td>28</td>
+                <td>29</td>
+                <td>30</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+    <h2>December 2022</h2>
+    <br />
+      
+    <table bgcolor="lightgrey" align="center" width="300"
+        cellspacing="21" cellpadding="21"> 
+        <caption align="top">
+        </caption>
+        <thead>
+            <tr>
+
+                <th>Sun</th>
+                <th>Mon</th>
+                <th>Tue</th>
+                <th>Wed</th>
+                <th>Thu</th>
+                <th>Fri</th>
+                <th>Sat</th>
+            </tr>
+        </thead>
+          
+        <tbody>
+            <tr>
+                <td> </td>
+                <td> </td>
+                <td></td>
+                <td></td>
+                <td>1</td>
+                <td>2</td>
+                <td>3</td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>5</td>
+                <td>6</td>
+                <td>7</td>
+                <td>8</td>
+                <td>9</td>
+                <td>10</td>
+            </tr>
+            <tr>
+                <td>11</td>
+                <td>12</td>
+                <td>13</td>
+                <td>14</td>
+                <td>15</td>
+                <td>16</td>
+                <td>17</td>
+            </tr>
+            <tr>
+                <td>18</td>
+                <td>19</td>
+                <td>20</td>
+                <td>21</td>
+                <td>22</td>
+                <td>23</td>
+                <td>24</td>
+            </tr>
+            <tr>
+                <td>25</td>
+                <td>26</td>
+                <td>27</td>
+                <td>28</td>
+                <td>29</td>
+                <td>30</td>
+                <td>31</td>
+            </tr>
+            </tbody>
+    </table>
+    </section>
+    <br />
+    <br />
+    <footer>
+      <div class="container">
+        <p>&copy; Lionico Barder Shop | Ayala Zamboanga City | 0905 188 4862</p>
+      </div>
+    </footer>
 </body>
 </html>
